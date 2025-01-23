ffc2d7b2b90ab761c3831590429b1f29d03f9493
zak <imzakbro@gmail.com>
2025-01-23 03:04:19 +0100

    server

--- controler_app/urls.py
+++ controler_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
 from .views import controler_app
 
-urlpatterns=[path('', controler_app)]
+app_name = "controller_app"
+urlpatterns = [path('', controler_app, name="press")]
--- controler_app/views.py
+++ controler_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+import pyautogui
+from orjson import loads
 
 def controler_app(request):
-	return render(request, 'controler_app/controler_app.html', {})
+    if request.method == "POST":
+        ky = loads(request.body)["ky"]
+        pyautogui.press(ky)
 
+    return render(request, 'controler_app/controler_app.html', {})
